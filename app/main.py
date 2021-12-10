ff6f3c8f80bc4761be1fa53e23b62cad64421096
cabeto <carlospenaaj@unimagdalena.edu.co>
2021-12-10 17:14:30 -0500

    Proyecto terminado

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+"""from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
  
 app = FastAPI()
@@ -9,6 +9,28 @@ app.add_middleware(
    allow_methods=["*"],
    allow_headers=["*"],
 )
+ 
+@app.get("/health-check")
+def health_check():
+   return "OK" """
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.api import api_router
+from app.database.session import engine, Base
+from app.core.config  import settings
+ 
+app = FastAPI()
+ 
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["http://localhost:8080"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
+ 
+app.include_router(api_router, prefix=settings.API_V1_STR)
  
 @app.get("/health-check")
 def health_check():
